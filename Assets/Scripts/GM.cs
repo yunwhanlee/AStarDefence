cff96859683c45d0e03319adef2877cb7796eb62
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2024-02-19 15:33:19 +0900

    A*AlgolismのStartがTopLeftとEndがBottomRightの時に、IndexのYがマイナスになりエラーが出るバグ対応。

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -20,9 +20,8 @@ public class GM : MonoBehaviour
 {
     public GameObject startPosObj;
     public GameObject targetPosObj;
-    public Vector2Int bottomLeft, topRight, startPos, targetPos;
+    public Vector2Int topLeft, bottomRight, startPos, targetPos;
     public List<Node> FinalNodeList;
-    public bool allowDiagonal, dontCrossCorner;
 
     int sizeX, sizeY;
     Node[,] NodeArray;
@@ -35,17 +34,18 @@ public class GM : MonoBehaviour
         //* Set Position
         Vector2Int start = new Vector2Int((int)Mathf.Round(startPosObj.transform.position.x), (int)Mathf.Round(startPosObj.transform.position.y));
         Vector2Int goal = new Vector2Int((int)Mathf.Round(targetPosObj.transform.position.x), (int)Mathf.Round(targetPosObj.transform.position.y));
-        bottomLeft = start;
+        topLeft = start;
         startPos = start;
-        topRight = goal;
+        bottomRight = goal;
         targetPos = goal;
     }
 
     public void PathFinding()
     {
         // NodeArray의 크기 정해주고, isWall, x, y 대입
-        sizeX = topRight.x - bottomLeft.x + 1;
-        sizeY = topRight.y - bottomLeft.y + 1;
+        sizeX = bottomRight.x - topLeft.x + 1;
+        sizeY = topLeft.y - bottomRight.y + 1;
+        Debug.Log("sizeX= " + sizeX + ", sizeY= " + sizeY);
         NodeArray = new Node[sizeX, sizeY];
 
         for (int i = 0; i < sizeX; i++)
@@ -53,17 +53,19 @@ public class GM : MonoBehaviour
             for (int j = 0; j < sizeY; j++)
             {
                 bool isWall = false;
-                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f))
+                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + topLeft.x, topLeft.y - j), 0.4f))
                     if (col.gameObject.layer == LayerMask.NameToLayer("Wall")) isWall = true;
 
-                NodeArray[i, j] = new Node(isWall, i + bottomLeft.x, j + bottomLeft.y);
+                NodeArray[i, j] = new Node(isWall, i + topLeft.x, topLeft.y - j);
             }
         }
         
 
         // 시작과 끝 노드, 열린리스트와 닫힌리스트, 마지막리스트 초기화
-        StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.y - bottomLeft.y];
-        TargetNode = NodeArray[targetPos.x - bottomLeft.x, targetPos.y - bottomLeft.y];
+        StartNode = NodeArray[startPos.x - topLeft.x, startPos.y - topLeft.y];
+        TargetNode = NodeArray[targetPos.x - topLeft.x, topLeft.y - targetPos.y];
+        Debug.Log("StartNode= " + (StartNode.x) + "," + (StartNode.y) + ", TargetNode= " + (TargetNode.x) + "," + (TargetNode.y));
+
 
         OpenList = new List<Node>() { StartNode };
         ClosedList = new List<Node>();
@@ -97,16 +99,6 @@ public class GM : MonoBehaviour
                 return;
             }
 
-
-            // ↗↖↙↘
-            if (allowDiagonal)
-            {
-                OpenListAdd(CurNode.x + 1, CurNode.y + 1);
-                OpenListAdd(CurNode.x - 1, CurNode.y + 1);
-                OpenListAdd(CurNode.x - 1, CurNode.y - 1);
-                OpenListAdd(CurNode.x + 1, CurNode.y - 1);
-            }
-
             // ↑ → ↓ ←
             OpenListAdd(CurNode.x, CurNode.y + 1);
             OpenListAdd(CurNode.x + 1, CurNode.y);
@@ -115,38 +107,46 @@ public class GM : MonoBehaviour
         }
     }
 
-    void OpenListAdd(int checkX, int checkY)
+void OpenListAdd(int checkX, int checkY)
+{
+    // 인덱스가 배열 범위를 벗어나는지 확인
+    if (checkX >= topLeft.x && checkX <= bottomRight.x && checkY >= bottomRight.y && checkY <= topLeft.y)
     {
-        // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
-        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
+        // 배열 내의 실제 인덱스 계산
+        int arrayX = checkX - topLeft.x;
+        int arrayY = topLeft.y - checkY;
+
+        // 벽이 아니고 닫힌 리스트에 포함되어 있지 않은 경우에만 처리
+        if (!NodeArray[arrayX, arrayY].isWall && !ClosedList.Contains(NodeArray[arrayX, arrayY]))
         {
-            // 대각선 허용시, 벽 사이로 통과 안됨
-            if (allowDiagonal) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+            Node NeighborNode = NodeArray[arrayX, arrayY];
+            int MoveCost = CurNode.G + ((CurNode.x - checkX == 0 || checkY - CurNode.y == 0) ? 10 : 14);
 
-            // 코너를 가로질러 가지 않을시, 이동 중에 수직수평 장애물이 있으면 안됨
-            if (dontCrossCorner) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
-
-            
-            // 이웃노드에 넣고, 직선은 10, 대각선은 14비용
-            Node NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
-            int MoveCost = CurNode.G + (CurNode.x - checkX == 0 || CurNode.y - checkY == 0 ? 10 : 14);
-
-
-            // 이동비용이 이웃노드G보다 작거나 또는 열린리스트에 이웃노드가 없다면 G, H, ParentNode를 설정 후 열린리스트에 추가
             if (MoveCost < NeighborNode.G || !OpenList.Contains(NeighborNode))
             {
                 NeighborNode.G = MoveCost;
-                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(NeighborNode.y - TargetNode.y)) * 10;
+                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(TargetNode.y - NeighborNode.y)) * 10;
                 NeighborNode.ParentNode = CurNode;
 
                 OpenList.Add(NeighborNode);
             }
         }
     }
+}
 
     void OnDrawGizmos()
     {
-        if(FinalNodeList.Count != 0) for (int i = 0; i < FinalNodeList.Count - 1; i++)
-                Gizmos.DrawLine(new Vector2(FinalNodeList[i].x, FinalNodeList[i].y), new Vector2(FinalNodeList[i + 1].x, FinalNodeList[i + 1].y));
+        if (FinalNodeList.Count >= 2) // FinalNodeList에 최소 2개 이상의 요소가 있어야 함
+        {
+            for (int i = 1; i < FinalNodeList.Count; i++) // i를 1부터 시작하도록 수정
+            {
+                // i가 0보다 큰지 검사하여 유효한 인덱스인지 확인
+                if (i > 0 && i < FinalNodeList.Count)
+                {
+                    // 이전 노드와 현재 노드 사이의 선을 그림
+                    Gizmos.DrawLine(new Vector2(FinalNodeList[i - 1].x, FinalNodeList[i - 1].y), new Vector2(FinalNodeList[i].x, FinalNodeList[i].y));
+                }
+            }
+        }
     }
 }
