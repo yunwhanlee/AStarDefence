861c55508b54127550b20f9886b0629525fa6b64
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2024-03-21 19:06:48 +0900

    Slow CcTower Sec減らす打つ速度も遅くにする。ボード無料数５個に変更。

--- Assets/Scripts/Public/Config.cs
+++ Assets/Scripts/Public/Config.cs
@@ -7,7 +7,7 @@ public static class Config{
     public readonly static Vector2Int START_POS = new Vector2Int(-8, 3);
     public readonly static Vector2Int GOAL_POS = new Vector2Int(8, -3);
 
-    public const int FREE_BOARD_CNT = 4;
+    public const int FREE_BOARD_CNT = 5;
     public const int FREE_BREAKROCK_CNT = 2;
 
     public const int DEFAULT_MONEY = 100;
@@ -22,7 +22,7 @@ public static class Config{
         public const int BOARD = 10;
         public const int TOWER = 15;
         public const int CCTOWER = 25;
-        public const int CC_UPG = 20;
+        public const int CC_UPG = 30;
         public const int MERGE = 5;
         public const float DELETE_REFUND_PER = 0.5f;
         public const int BOSS_KILL_BONUS = 65;
