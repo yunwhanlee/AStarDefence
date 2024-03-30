5028d219883fc2df19d07bda01f129c7430bbdc2
yunwhanlee <yunwhanlee@gmail.com>
2024-03-30 20:46:59 +0900

    SkillTreeUIManager PopUpイベント基本部分適用

--- Assets/Scripts/Home/SkillTree/SkillTreeUIManager.cs
+++ Assets/Scripts/Home/SkillTree/SkillTreeUIManager.cs
@@ -32,26 +32,33 @@ public class SkillTreeUIManager : MonoBehaviour {
     [field:SerializeField] public SkillTree[] MagicianSkillTrees {get; private set;}
     [field:SerializeField] public SkillTree[] UtilitySkillTrees {get; private set;}
 
-
     //* UI
+    [field:SerializeField] public GameObject WindowObj {get; private set;}
     [field:SerializeField] public TextMeshProUGUI MySkillPointTxt {get; private set;}
-
     [field:SerializeField] public Image SkillIconBgImg {get; private set;}
     [field:SerializeField] public Image SkillIconImg {get; private set;}
     [field:SerializeField] public TextMeshProUGUI SkillNameTxt {get; private set;}
     [field:SerializeField] public TextMeshProUGUI SkillInfoTxt {get; private set;}
-    [field:SerializeField] public TextMeshProUGUI SkillSpendPointTxt {get; private set;}
+    [field:SerializeField] public TextMeshProUGUI NeededSkillPointTxt {get; private set;}
 
-    int i = 0;
-    void Update() {
-        if(Input.GetKeyDown(KeyCode.V)) {
-            i++;
-            if(i > 3) i = 0;
-            BgPatternCtrl.BgImg.color = BgPatternCtrl.SkillCateColors[i];
-        }
+    void Start() {
+        OnClickWarriorSkillTreeBtn(0);
     }
 
     #region EVENT
+        public void OnClickSkillTreeIconBtn() {
+            WindowObj.SetActive(true);
+        }
+        public void OnClickClosePopUpBtn() {
+            WindowObj.SetActive(false);
+        }
+        public void OnClickResetSkillPointBtn() {
+            Debug.Log("RESET SKILL POINT");
+        }
+        public void OnClickLearnSkillBtn() {
+            Debug.Log("LEARN SKILL");
+        }
+
         public void OnClickWarriorSkillTreeBtn(int idx) {
             SetUI(idx, SkillTreeCate.Warrior, WarriorSkillTrees, WarriorSkillTreeSO);
         }
@@ -68,12 +75,15 @@ public class SkillTreeUIManager : MonoBehaviour {
 
     #region FUNC
         private void SetUI(int idx, SkillTreeCate cate, SkillTree[] skillTrees, SettingSkillTreeData skillTreeDataSO) {
+            //* 背景とアイコン背景色
+            BgPatternCtrl.BgImg.color = BgPatternCtrl.SkillCateColors[(int)cate];
             SkillIconBgImg.color = SkillIconBgColors[(int)cate];
+
             SkillIconImg.sprite = skillTrees[idx].IconImg.sprite;
 
             SkillNameTxt.text = skillTreeDataSO.Datas[idx].Name;
             SkillInfoTxt.text = skillTreeDataSO.Datas[idx].Description;
-            SkillSpendPointTxt.text = $"{WarriorSkillTreeSO.Datas[idx].Cost}";
+            NeededSkillPointTxt.text = $"{WarriorSkillTreeSO.Datas[idx].Cost}";
         }
     #endregion
 }
