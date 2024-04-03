5d379f8e763a4b0997464f5e82108a63866822cb
yunwhanlee <yunwhanlee@gmail.com>
2024-04-03 21:28:05 +0900

    RewardItem クラスバグあったので再生成

--- Assets/Scripts/Home/Reward/HomeRewardUIManager.cs
+++ Assets/Scripts/Home/Reward/HomeRewardUIManager.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Inventory.Model;
 using UnityEngine;
 
+[Serializable]
+public class RewardItem {
+    [field: SerializeField] public ItemSO InventoryItem {get; private set;}
+    [field: SerializeField] public int Val {get; set;} = 1;
+}
+
 public class HomeRewardUIManager : MonoBehaviour {
     public List<RewardItem> RewardList;
     [SerializeField] private InventorySO InventoryData;
-
-
 
     void Update() {
         if(Input.GetKeyDown(KeyCode.A)) {
