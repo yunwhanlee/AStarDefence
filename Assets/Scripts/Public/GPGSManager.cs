d883cf7aac837919594fc72a5783b81e7d538087
yunwhanlee <yunwhanlee@gmail.com>
2024-05-07 01:22:36 +0900

    GPGS Login 一回のみにして、ロードしますというメッセージ表示。

--- Assets/Scripts/Public/GPGSManager.cs
+++ Assets/Scripts/Public/GPGSManager.cs
@@ -9,13 +9,16 @@ using UnityEngine.SocialPlatforms;
 public class GPGSManager : MonoBehaviour {
     public static GPGSManager _;
 
+    bool isLogin = false;
+
     void Start() {
         _ = this;
     }
 
 #region EVENT
     public void OnClickRankIconBtnAtHome() {
-        GPGSLogin();
+        if(!isLogin)
+            GPGSLogin();
         ShowLeaderBoardUI();
     }
 #endregion
@@ -27,9 +30,10 @@ public class GPGSManager : MonoBehaviour {
 
     internal void ProcessAuthentication(SignInStatus status) {
         if (status == SignInStatus.Success) {
+            isLogin = true;
             string displayName = PlayGamesPlatform.Instance.GetUserDisplayName();
             string userID = PlayGamesPlatform.Instance.GetUserId();
-            HM._.hui.ShowMsgNotice($"로그인 성공: {displayName} / {userID}");
+            HM._.hui.ShowMsgNotice($"로그인 성공: 구글 리더보드를 불러옵니다.");
         } else {
             HM._.hui.ShowMsgNotice("로그인 실패");
         }
