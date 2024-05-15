d83712aed740309b2325e02f28dfb72794f49094
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2024-05-15 19:03:24 +0900

    BonusRwdBubbleUI デザイン設定中。

--- Assets/Scripts/Home/BonusReward/BonusRwdBubbleUI.cs
+++ Assets/Scripts/Home/BonusReward/BonusRwdBubbleUI.cs
@@ -6,21 +6,53 @@ using UnityEngine.UI;
 
 public class BonusRwdBubbleUI : MonoBehaviour {
     [field: SerializeField] public Sprite PointMarkBlueSpr {get; set;}
-    [field: SerializeField] public Sprite PointMarkSprYellow {get; set;}
-
+    [field: SerializeField] public Sprite PointMarkYellowSpr {get; set;}
+    [field: SerializeField] public Color HalfTransparentClr {get; set;}
+    [field: SerializeField] public Color originClr {get; set;}
+    [field: SerializeField] public Color BubbleBlueClr {get; set;}
+    [field: SerializeField] public Color UnlockGrayFrameClr {get; set;}
+    [field: SerializeField] public Color UnlockOrangeFrameClr {get; set;}
 
     [field: SerializeField] public string Name {get; set;}
     [field: SerializeField] public int Id {get; set;}
     [field: SerializeField] public int Quantity {get; set;}
-    [field: SerializeField] public string UnlockCnt {get; set;}
+    [field: SerializeField] public int UnlockCnt {get; set;}
     [field: SerializeField] public Image PointMarkImg {get; set;}
-    [field: SerializeField] public Image NeedCntFrameImg {get; set;}
-    [field: SerializeField] public TMP_Text NeedCntTxt {get; set;}
+    [field: SerializeField] public Image UnlockCntFrameImg {get; set;}
     [field: SerializeField] public Image BubbleFrameImg {get; set;}
     [field: SerializeField] public Image BubbleArrowImg {get; set;}
     [field: SerializeField] public Image ItemIconImg {get; set;}
     [field: SerializeField] public TMP_Text ItemNameTxt {get; set;}
+    [field: SerializeField] public TMP_Text UnlockCntTxt {get; set;}
     [field: SerializeField] public Image CheckMark {get; set;}
     [field: SerializeField] public Image LockMark {get; set;}
 
+    private void SetUIStyle(Color bubbleClr, Color txtClr, Sprite pointMarkSpr, Color unlockFrameClr) {
+        BubbleFrameImg.color = bubbleClr;
+        BubbleArrowImg.color = bubbleClr;
+        ItemIconImg.color = txtClr;
+        ItemNameTxt.color = txtClr;
+        PointMarkImg.sprite = pointMarkSpr;
+        UnlockCntFrameImg.color = unlockFrameClr;
+    }
+
+    public void SetData(int id, string name, int quantity, int unlockCnt) {
+        Id = id;
+        Name = name;
+        Quantity = quantity;
+        UnlockCnt = unlockCnt;
+    }
+
+    public void SetUI() {
+        ItemNameTxt.text = $"{Name}";
+        UnlockCntTxt.text = $"{UnlockCnt}";
+
+        //* Locked
+        SetUIStyle(BubbleBlueClr, HalfTransparentClr, PointMarkBlueSpr, UnlockGrayFrameClr);
+
+        //* Unlocked
+        SetUIStyle(originClr, originClr, PointMarkYellowSpr, UnlockOrangeFrameClr);
+
+        //* Accepted
+    }
 }
--- Assets/Scripts/Home/BonusReward/MileageRewardUIManager.cs
+++ Assets/Scripts/Home/BonusReward/MileageRewardUIManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Inventory.Model;
 using UnityEngine;
 
-public class MileageRewardUIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+[System.Serializable]
+public class RwdBubbleDt {
+    [field:SerializeField] public int UnlockCnt {get; set;}
+    [field:SerializeField] public ItemSO ItemDt {get; set;}
+}
+
+public class MileageRewardUIManager : MonoBehaviour {
+    [field:SerializeField] public RwdBubbleDt[] RwdBubbleDts {get; private set;}
+
+    [field:SerializeField] public Transform BubbleGroupTf {get; private set;}
+    [field:SerializeField] public GameObject RwdItemBubblePf {get; private set;}
+
+    void Start() {
         
+        int i = 0;
+        foreach (var bubbleDt in RwdBubbleDts) {
+            var rwdBubbleUI = Instantiate(RwdItemBubblePf, BubbleGroupTf).GetComponent<BonusRwdBubbleUI>();
+            rwdBubbleUI.ItemIconImg.sprite = bubbleDt.ItemDt.ItemImg;
+
+            //* タイプ名
+            string typeName = (bubbleDt.ItemDt.Type == Enum.ItemType.Weapon)? "무기"
+                : (bubbleDt.ItemDt.Type == Enum.ItemType.Shoes)? "신발"
+                : (bubbleDt.ItemDt.Type == Enum.ItemType.Ring)? "반지"
+                :  "유물";
+
+            //* 設定
+            rwdBubbleUI.SetData(i, typeName, 1, bubbleDt.UnlockCnt);
+            rwdBubbleUI.SetUI();
+
+            i++;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
