42297580417e2ff337aded2ba07a280ea5cb9dbf
yunwhanlee <yunwhanlee@gmail.com>
2024-06-13 04:00:36 +0900

    Infinite Dungeon HPRatio と Flight /3 -> /2 と Boss Cycle X 5 -> X 9

--- Assets/Editor/SettingEnemyDataEditor.cs
+++ Assets/Editor/SettingEnemyDataEditor.cs
@@ -144,52 +144,52 @@ public class SettingEnemyDataEditor : Editor {
 
                 //* Hp 比率
                 if(i == 0) hpRatio = 1.5f;
-                if(i == 1) hpRatio = 1.4f;
-                if(i == 2) hpRatio = 1.375f;
-                if(i == 3) hpRatio = 1.35f;
-                if(i == 4) hpRatio = 1.325f;
-                if(i == 5) hpRatio = 1.3f;
-                if(i == 6) hpRatio = 1.28f;
-                if(i == 7) hpRatio = 1.25f;
-                if(i == 8) hpRatio = 1.225f;
-                if(i == 9) hpRatio = 1.2f;
-                if(i == 10) hpRatio = 1.19f;
-                if(i == 11) hpRatio = 1.18f;
-                if(i == 12) hpRatio = 1.17f;
-                if(i == 13) hpRatio = 1.17f;
-                if(i == 14) hpRatio = 1.16f;
-                if(i == 15) hpRatio = 1.15f;
-                if(i == 16) hpRatio = 1.13f;
-                if(i == 17) hpRatio = 1.12f;
-                if(i == 18) hpRatio = 1.11f;
-                if(i == 19) hpRatio = 1.1f;
-                if(i == 20) hpRatio = 1.085f;
-                if(i == 21) hpRatio = 1.078f;
-                if(i == 22) hpRatio = 1.08f;
-                if(i == 23) hpRatio = 1.073f;
-                if(i == 24) hpRatio = 1.075f;
-                if(i == 25) hpRatio = 1.07f;
-                if(i == 26) hpRatio = 1.07f;
-                if(i == 27) hpRatio = 1.07f;
-                if(i == 28) hpRatio = 1.06f;
-                if(i == 29) hpRatio = 1.05f;
-                if(i == 39) hpRatio = 1.045f;
-                if(i > 39 && i <= 49) hpRatio = 1.041f;
-                if(i > 49 && i <= 59) hpRatio = 1.0375f;
-                if(i > 59 && i <= 69) hpRatio = 1.033f;
-                if(i > 69 && i <= 79) hpRatio = 1.031f;
-                if(i > 79 && i <= 89) hpRatio = 1.028f;
-                if(i > 89 && i <= 99) hpRatio = 1.024f;
-                if(i > 99 && i <= 139) hpRatio = 1.02f;
-                if(i > 139 && i <= 179) hpRatio = 1.018f;
-                if(i > 179 && i <= 209) hpRatio = 1.014f;
-                if(i > 209 && i <= 309) hpRatio = 1.012f;
-                if(i > 309 && i <= 409) hpRatio = 1.009f;
-                if(i > 409 && i <= 509) hpRatio = 1.0078f;
-                if(i > 509 && i <= 609) hpRatio = 1.0065f;
-                if(i > 609 && i <= 709) hpRatio = 1.0057f;
-                if(i > 709 && i <= 809) hpRatio = 1.0045f;
-                else if(i > 209 && i <= WaveCnt) hpRatio = 1.0038f;
+                if(i == 1) hpRatio = 1.45f;
+                if(i == 2) hpRatio = 1.425f;
+                if(i == 3) hpRatio = 1.3985f;
+                if(i == 4) hpRatio = 1.375f;
+                if(i == 5) hpRatio = 1.35f;
+                if(i == 6) hpRatio = 1.325f;
+                if(i == 7) hpRatio = 1.29875f;
+                if(i == 8) hpRatio = 1.26875f;
+                if(i == 9) hpRatio = 1.25f;
+                if(i == 10) hpRatio = 1.24f;
+                if(i == 11) hpRatio = 1.23f;
+                if(i == 12) hpRatio = 1.23f;
+                if(i == 13) hpRatio = 1.22f;
+                if(i == 14) hpRatio = 1.21f;
+                if(i == 15) hpRatio = 1.205f;
+                if(i == 16) hpRatio = 1.2f;
+                if(i == 17) hpRatio = 1.1875f;
+                if(i == 18) hpRatio = 1.16f;
+                if(i == 19) hpRatio = 1.15f;
+                if(i == 20) hpRatio = 1.14f;
+                if(i == 21) hpRatio = 1.13f;
+                if(i == 22) hpRatio = 1.11f;
+                if(i == 23) hpRatio = 1.1f;
+                if(i == 24) hpRatio = 1.098f;
+                if(i == 25) hpRatio = 1.095f;
+                if(i == 26) hpRatio = 1.092f;
+                if(i == 27) hpRatio = 1.09f;
+                if(i == 28) hpRatio = 1.0895f;
+                if(i == 29) hpRatio = 1.08875f;
+                if(i == 39) hpRatio = 1.08687f;
+                if(i > 39 && i <= 49) hpRatio   = 1.081f;
+                if(i > 49 && i <= 59) hpRatio   = 1.0745f;
+                if(i > 59 && i <= 69) hpRatio   = 1.067f;
+                if(i > 69 && i <= 79) hpRatio   = 1.061f;
+                if(i > 79 && i <= 89) hpRatio   = 1.057f;
+                if(i > 89 && i <= 99) hpRatio   = 1.051f;
+                if(i > 99 && i <= 139) hpRatio  = 1.0468f;
+                if(i > 139 && i <= 179) hpRatio = 1.0437f;
+                if(i > 179 && i <= 209) hpRatio = 1.0418f;
+                if(i > 209 && i <= 309) hpRatio = 1.0375f;
+                if(i > 309 && i <= 409) hpRatio = 1.0329f;
+                if(i > 409 && i <= 509) hpRatio = 1.02651f;
+                if(i > 509 && i <= 609) hpRatio = 1.0213f;
+                if(i > 609 && i <= 709) hpRatio = 1.0186f;
+                if(i > 709 && i <= 809) hpRatio = 1.0148f;
+                else if(i > 209 && i <= WaveCnt) hpRatio = 1.0115f;
 
                 hp = Mathf.RoundToInt(hp * hpRatio);
 
@@ -198,12 +198,12 @@ public class SettingEnemyDataEditor : Editor {
                 const int OneCycle = Config.BOSS_SPAWN_CNT;
                 if(enemyIdx % OneCycle == Config.FLIGHT_SPAWN_CNT) {
                     dt.Waves[i].Type = EnemyType.Flight;
-                    dt.Waves[i].Hp = hp / 3;
+                    dt.Waves[i].Hp = hp / 2;
                     dt.Waves[i].Speed = 1.5f;
                 }
                 else if(enemyIdx % OneCycle == 0) {
                     dt.Waves[i].Type = EnemyType.Boss;
-                    dt.Waves[i].Hp = hp * (StartBossHpTime + (enemyIdx % Config.BOSS_SPAWN_CNT * 5));
+                    dt.Waves[i].Hp = hp * (StartBossHpTime + (enemyIdx % Config.BOSS_SPAWN_CNT * 9));
                     dt.Waves[i].Speed = 1;
                 }
                 else {
