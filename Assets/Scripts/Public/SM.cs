22f718f4253dee2cbe64034bbf104f7683392e56
yunwhanlee <yunwhanlee@gmail.com>
2024-06-15 01:44:44 +0900

    SM:: SfxSound 分けてチャンネル管理

--- Assets/Scripts/Public/SM.cs
+++ Assets/Scripts/Public/SM.cs
@@ -4,6 +4,51 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR;
 
+[System.Serializable]
+/// <summary>
+/// SFXサウンドを分けてチャンネル管理
+/// </summary>
+public class SfxAudio {
+    [field: SerializeField] public GameObject Obj {get; set;}
+    [field: SerializeField] public AudioSource[] Players {get; set;}
+    [field: SerializeField] public float Volume {get; set;}
+    [field: SerializeField] public int Channels {get; set;}
+    [field: SerializeField] public int ChannelIdx {get; set;}
+
+    public SfxAudio(string objName, int channelCnt, float volume) {
+        Channels = channelCnt;
+        Volume = volume;
+        ChannelIdx = 0;
+
+        Obj = new GameObject(objName);
+        Obj.transform.parent = SM._.transform;
+        Players = new AudioSource[Channels];
+
+        for(int i = 0; i < Players.Length; i++) {
+            Players[i] = Obj.AddComponent<AudioSource>();
+            Players[i].playOnAwake = false;
+            Players[i].volume = Volume;
+        }
+    }
+
+    public void PlayClip(AudioClip clip) {
+        for(int i = 0; i < Players.Length; i++) {
+            int loopIdx = (i + ChannelIdx) % Players.Length;
+
+            //* 現在のAudioPlayerが既に再生中なら、次に進む
+            if(Players[loopIdx].isPlaying)
+                continue;
+
+            //* 空のAutioPlayerへClip再生
+            ChannelIdx = loopIdx;
+            Players[loopIdx].clip = clip;
+            Players[loopIdx].Play();
+            Debug.Log($"{Obj.name}= SfxClips[{Players[loopIdx].clip.name}]");
+            break; // ループ終了
+        }
+    }
+}
+
 public class SM : MonoBehaviour {
     static public SM _;
 
@@ -100,17 +145,23 @@ public class SM : MonoBehaviour {
     [field: SerializeField] AudioClip GoblinDungeonBGM {get; set;}
 
     [field: Header("SFX")]
-    [field: SerializeField] public GameObject SfxObj {get; set;}
-    [field: SerializeField] public AudioSource[] SfxPlayers {get; set;}
     [field: SerializeField] AudioClip[] SfxClips {get; set;}
-    [field: SerializeField] public float SfxVolume {get; set;}
-    [field: SerializeField] public int Channels {get; set;}
-    [field: SerializeField] public int ChannelIdx {get; set;}
+    [field: SerializeField] public SfxAudio NormalSfx {get; set;}
+    [field: SerializeField] public SfxAudio HitSfx {get; set;}
+    [field: SerializeField] public SfxAudio AtkSfx {get; set;}
+
+    [field: Header("HIT SFX")]
+    [field: SerializeField] public GameObject HitSfxObj {get; set;}
+    [field: SerializeField] public AudioSource[] HitSfxPlayers {get; set;}
+    [field: SerializeField] AudioClip[] HitSfxClips {get; set;}
+    [field: SerializeField] public int HitChannels {get; set;}
+    [field: SerializeField] public int HitChannelIdx {get; set;}
+
 
     void Awake() {
         Singleton();
         InitSfx();
-    } 
+    }
 
     private void Singleton(){
         if(_ == null) {
@@ -127,15 +178,9 @@ public class SM : MonoBehaviour {
 
 #region FUNC
     private void InitSfx() {
-        SfxObj = new GameObject("SfxPlayer");
-        SfxObj.transform.parent = transform;
-        SfxPlayers = new AudioSource[Channels];
-
-        for(int i = 0; i < SfxPlayers.Length; i++) {
-            SfxPlayers[i] = SfxObj.AddComponent<AudioSource>();
-            SfxPlayers[i].playOnAwake = false;
-            SfxPlayers[i].volume = SfxVolume;
-        }
+        NormalSfx = new SfxAudio("SfxPlayer", 40, 0.5f);
+        AtkSfx = new SfxAudio("AtkSfxPlayer", 40, 0.5f);
+        HitSfx = new SfxAudio("HitSfxPlayer", 40, 0.5f);
     }
 
     public void SetVolumeBGM(float volume) {
@@ -144,11 +189,13 @@ public class SM : MonoBehaviour {
         BgmPlay(BGM.HomeBGM);
     }
     public void SetVolumeSFX(float volume) {
-        SfxVolume = volume;
-        for(int i = 0; i < SfxPlayers.Length; i++) {
-            SfxPlayers[i].volume = volume;
-        }
-        SfxObj.SetActive(volume > 0);
+        DM._.DB.SettingDB.SfxVolume = volume;
+        for(int i = 0; i < NormalSfx.Players.Length; i++) NormalSfx.Players[i].volume = volume;
+        for(int i = 0; i < HitSfx.Players.Length; i++) HitSfx.Players[i].volume = volume;
+        for(int i = 0; i < AtkSfx.Players.Length; i++) AtkSfx.Players[i].volume = volume;
+        NormalSfx.Obj.SetActive(volume > 0);
+        HitSfx.Obj.SetActive(volume > 0);
+        AtkSfx.Obj.SetActive(volume > 0);
     }
 
     private void SetBGM(AudioClip bgm) {
@@ -184,110 +231,34 @@ public class SM : MonoBehaviour {
 
     private IEnumerator CoSfxPlay(SFX sfx, float delay) {
         // //* ON・OFF チェック
-        // if(!SfxPlayers.gameObject.activeSelf)
-            // yield break;
+        if(!NormalSfx.Obj.activeSelf)
+            yield break;
 
         //* Delay
         yield return new WaitForSeconds(delay);
 
-        for(int i = 0; i < SfxPlayers.Length; i++) {
-            int loopIdx = (i + ChannelIdx) % SfxPlayers.Length;
-
-            //* 現在のAudioPlayerが既に再生中なら、次に進む
-            if(SfxPlayers[loopIdx].isPlaying)
-                continue;
-
-            //* 空のAutioPlayerへClip再生
-            ChannelIdx = loopIdx;
-
-            switch(sfx) {
-                case SFX.HitSFX:
-                case SFX.HitSFX2:
-                case SFX.HitSFX3:
-                    SfxPlayers[loopIdx].clip = SfxClips[Random.Range((int)SFX.HitSFX, (int)SFX.HitSFX3 + 1)];
-                    break;
-                case SFX.MetalHitSFX:
-                case SFX.MetalHitSFX2:
-                    SfxPlayers[loopIdx].clip = SfxClips[Random.Range((int)SFX.MetalHitSFX, (int)SFX.MetalHitSFX2 + 1)];
-                    break;
-                case SFX.EnemyDeadSFX:
-                case SFX.EnemyDeadSFX2:
-                    SfxPlayers[loopIdx].clip = SfxClips[Random.Range((int)SFX.EnemyDeadSFX, (int)SFX.EnemyDeadSFX2 + 1)];
-                    break;
-                default:
-                    SfxPlayers[loopIdx].clip = SfxClips[(int)sfx];
-                    break;
-            }
-
-            
-            SfxPlayers[loopIdx].Play();
-            Debug.Log($"SfxPlayers[{loopIdx}]= SfxClips[{SfxPlayers[loopIdx].clip.name}]");
-            break; // ループ終了
+        switch(sfx) {
+            case SFX.HitSFX: case SFX.HitSFX2: case SFX.HitSFX3:
+            case SFX.MetalHitSFX: case SFX.MetalHitSFX2:
+            case SFX.EnemyDeadSFX: case SFX.EnemyDeadSFX2:
+            case SFX.BossKilledSFX:
+                AudioClip clip = (sfx == SFX.HitSFX || sfx == SFX.HitSFX2 || sfx == SFX.HitSFX)? SfxClips[Random.Range((int)SFX.HitSFX, (int)SFX.HitSFX3 + 1)]
+                    : (sfx == SFX.MetalHitSFX || sfx == SFX.MetalHitSFX2)? SfxClips[Random.Range((int)SFX.MetalHitSFX, (int)SFX.MetalHitSFX2 + 1)]
+                    : (sfx == SFX.EnemyDeadSFX || sfx == SFX.EnemyDeadSFX2)? SfxClips[Random.Range((int)SFX.EnemyDeadSFX, (int)SFX.EnemyDeadSFX2 + 1)]
+                    : null;
+                HitSfx.PlayClip(clip);
+                break;
+            case SFX.SwordSFX: case SFX.ArrowSFX: 
+            case SFX.Magic1SFX: case SFX.Magic2SFX: case SFX.Magic3SFX: 
+            case SFX.Magic4SFX: case SFX.Magic5SFX: case SFX.Magic6SFX:
+            case SFX.CCFrostSFX: case SFX.CCLightningSFX:
+            case SFX.FrostNovaSFX: case SFX.LightningNovaSFX: 
+                AtkSfx.PlayClip(SfxClips[(int)sfx]);
+                break;
+            default:
+                NormalSfx.PlayClip(SfxClips[(int)sfx]);
+                break;
         }
-
-
-
-        // //* GAME
-        // if(sfx == SFX.GameStartSFX) SfxPlayers.PlayOneShot(GameStartSFX);
-        // if(sfx == SFX.WaveStartSFX) SfxPlayers.PlayOneShot(WaveStartSFX);
-        // if(sfx == SFX.GameoverSFX) SfxPlayers.PlayOneShot(GameoverSFX);
-        // if(sfx == SFX.UpgradeSFX) SfxPlayers.PlayOneShot(UpgradeSFX);
-        // if(sfx == SFX.CreateTowerSFX) SfxPlayers.PlayOneShot(CreateTowerSFX);
-        // if(sfx == SFX.DeleteTowerSFX) SfxPlayers.PlayOneShot(DeleteTowerSFX);
-        // if(sfx == SFX.BuildSFX) SfxPlayers.PlayOneShot(BuildSFX);
-        // if(sfx == SFX.BreakSFX) SfxPlayers.PlayOneShot(BreakSFX);
-        // if(sfx == SFX.Merge1SFX) SfxPlayers.PlayOneShot(Merge1SFX);
-        // if(sfx == SFX.Merge2SFX) SfxPlayers.PlayOneShot(Merge2SFX);
-        // if(sfx == SFX.Merge3SFX) SfxPlayers.PlayOneShot(Merge3SFX);
-        // if(sfx == SFX.Merge4SFX) SfxPlayers.PlayOneShot(Merge4SFX);
-        // if(sfx == SFX.Merge5SFX) SfxPlayers.PlayOneShot(Merge5SFX);
-        // if(sfx == SFX.DecreaseLife) SfxPlayers.PlayOneShot(DecreaseLife);
-        // // Attack
-        // if(sfx == SFX.SwordSFX) SfxPlayers.PlayOneShot(SwordSFX);
-        // if(sfx == SFX.ArrowSFX) SfxPlayers.PlayOneShot(ArrowSFX);
-        // if(sfx == SFX.Magic1SFX) SfxPlayers.PlayOneShot(Magic1SFX);
-        // if(sfx == SFX.Magic2SFX) SfxPlayers.PlayOneShot(Magic2SFX);
-        // if(sfx == SFX.Magic3SFX) SfxPlayers.PlayOneShot(Magic3SFX);
-        // if(sfx == SFX.Magic4SFX) SfxPlayers.PlayOneShot(Magic4SFX);
-        // if(sfx == SFX.Magic5SFX) SfxPlayers.PlayOneShot(Magic5SFX);
-        // if(sfx == SFX.Magic6SFX) SfxPlayers.PlayOneShot(Magic6SFX);
-        // if(sfx == SFX.CCFrostSFX) SfxPlayers.PlayOneShot(CCFrostSFX);
-        // if(sfx == SFX.CCLightningSFX) SfxPlayers.PlayOneShot(CCLightningSFX);
-        // if(sfx == SFX.FrostNovaSFX) SfxPlayers.PlayOneShot(FrostNovaSFX);
-        // if(sfx == SFX.LightningNovaSFX) SfxPlayers.PlayOneShot(LightningNovaSFX);
-        // // Hit
-        // if(sfx == SFX.HitSFX) SfxPlayers.PlayOneShot(HitSFXs[Random.Range(0, HitSFXs.Length)]);
-        // if(sfx == SFX.MetalHitSFX) SfxPlayers.PlayOneShot(MetalHitSFXs[Random.Range(0, MetalHitSFXs.Length)]);
-        // if(sfx == SFX.EnemyDeadSFX) SfxPlayers.PlayOneShot(EnemyDeadSFXs[Random.Range(0, EnemyDeadSFXs.Length)]);
-        // if(sfx == SFX.BossKilledSFX) SfxPlayers.PlayOneShot(BossKilledSFX);
-        // // Skill
-        // if(sfx == SFX.RageSFX) SfxPlayers.PlayOneShot(RageSFX);
-        // if(sfx == SFX.CheerUpSFX) SfxPlayers.PlayOneShot(CheerUpSFX);
-        // if(sfx == SFX.WheelWindSFX) SfxPlayers.PlayOneShot(WheelWindSFX);
-        // if(sfx == SFX.RoarASFX) SfxPlayers.PlayOneShot(RoarASFX);
-        // if(sfx == SFX.RoarBSFX) SfxPlayers.PlayOneShot(RoarBSFX);
-        // if(sfx == SFX.FireExplosionSFX) SfxPlayers.PlayOneShot(FireExplosionSFX);
-        // if(sfx == SFX.MagicCircle1SFX) SfxPlayers.PlayOneShot(MagicCircle1SFX);
-        // if(sfx == SFX.MagicCircle2SFX) SfxPlayers.PlayOneShot(MagicCircle2SFX);
-        // if(sfx == SFX.MagicCircle3SFX) SfxPlayers.PlayOneShot(MagicCircle3SFX);
-        // if(sfx == SFX.LaserSFX) SfxPlayers.PlayOneShot(LaserSFX);
-        // if(sfx == SFX.BigbangSFX) SfxPlayers.PlayOneShot(BigbangSFX);
-        // if(sfx == SFX.PassArrowSFX) SfxPlayers.PlayOneShot(PassArrowSFX);
-        // if(sfx == SFX.ArrowRainSFX) SfxPlayers.PlayOneShot(ArrowRainSFX);
-
-        // //* UI   
-        // if(sfx == SFX.RewardSFX) SfxPlayers.PlayOneShot(RewardSFX);
-        // if(sfx == SFX.ClickSFX) SfxPlayers.PlayOneShot(ClickSFX);
-        // if(sfx == SFX.CompleteSFX) SfxPlayers.PlayOneShot(CompleteSFX);
-        // if(sfx == SFX.ErrorSFX) SfxPlayers.PlayOneShot(ErrorSFX);
-        // if(sfx == SFX.StageSelectSFX) SfxPlayers.PlayOneShot(StageSelectSFX);
-        // if(sfx == SFX.ItemPickSFX) SfxPlayers.PlayOneShot(ItemPickSFX);
-        // if(sfx == SFX.InvEquipSFX) SfxPlayers.PlayOneShot(InvEquipSFX);
-        // if(sfx == SFX.InvUnEquipSFX) SfxPlayers.PlayOneShot(InvUnEquipSFX);
-        // if(sfx == SFX.InvStoneSFX) SfxPlayers.PlayOneShot(InvStoneSFX);
-        // if(sfx == SFX.LevelUpSFX) SfxPlayers.PlayOneShot(LevelUpSFX);
-        // if(sfx == SFX.BossSpawnSFX) SfxPlayers.PlayOneShot(BossSpawnSFX);
-        // if(sfx == SFX.CountdownSFX) SfxPlayers.PlayOneShot(CountdownSFX);
     }
 #endregion
 }
